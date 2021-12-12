1883ed9b1be6f5d12b1774892e464224ce354962
OldWhaler <savin.mailbox@yandex.ru>
2021-12-12 18:38:49 +0300

    add gitignore

--- unit_7/test.js
+++ unit_7/test.js
@@ -7,5 +7,8 @@ const someText = 'Some text'
 // fs.writeFileSync('test.txt', someText)
 // console.log(fs.readFileSync('test.txt', 'utf-8'))
 
-const someArr = fs.readdirSync('../')
-console.log(someArr)
+// const someArr = fs.readdirSync('../')
+// console.log(someArr)
+
+const j3 = require('./j3.json')
+console.log(j3)
